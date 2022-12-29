01c1fb4be51d81073a229ed14951828235b9f4db
farruk98f <farrukhfayzullaev98@gmail.com>
2022-12-29 14:34:40 +0900

    1st

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "@next/font/google"
+import styles from "../styles/Home.module.css"
+import Layout from "../components/Layout"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <h1 className="text-3xl text-green-500 bg-red-500">My first web page</h1>
+      <p>Author: Fayzullaev.F</p>
+    </Layout>
+  )
+}
